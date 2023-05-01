37e5f008db4bbb276c7a23f02243a892e613a36d
pheonix2121 <bobbypanwar73@gmail.com>
2023-05-01 11:12:35 +0530

    project 1

---
+++ my-files/src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItems from "./ExpenseItems";
+
+function App() {
+  return (
+    <div>
+      <h1>ExpenseItems</h1>
+      <ExpenseItems></ExpenseItems>
+    </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ my-files/src/ExpenseItems.js
@@ -0,0 +1,15 @@
+function ExpenseItems() {
+    return (
+      <div>
+        
+         <div/>
+         
+         <div>Food Rs10</div>
+         <div>Petrol Rs20</div>
+         <div>Movies Rs 200</div>
+         <div/>
+         </div>
+    );
+  };
+  
+  export default ExpenseItems;
